58fb604074854be2b11a5c824456be83262ed542
leoneleg <leoneleg@ext.inditex.com>
2022-01-16 18:38:52 +0100

    init project

---
+++ src/layout/Main.js
@@ -0,0 +1,12 @@
+export default function Main() {
+    return (
+        <div>
+            <h1>Soda2</h1>
+            <p>
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+        </div>
+    );
+}
+
+
---
+++ src/layout/Menu.js
@@ -0,0 +1,14 @@
+export default function Menu() {
+    return (
+        <div>            
+            <ul>
+                <li><a href="#list">Listado</a></li>
+                <li><a href="#delivery">Repartidores</a></li>
+                <li><a href="#print">Imprimir</a></li>
+                <li><a href="#search">Buscar</a></li>
+                <li><a href="#export">Exportar</a></li>
+                <li><a href="#exit">Salir</a></li>
+            </ul>
+        </div>
+    );
+}
